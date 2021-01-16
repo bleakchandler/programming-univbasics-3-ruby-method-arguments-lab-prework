8627703ead25c5c4793e82100430a4123f8814fe
Bleak Chandler <bleakchandler@gmail.com>
2021-01-16 23:56:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,7 @@ def introduction_with_language (name, language = 'Ruby')
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end  
 
-  introduction_with_language("Bleak", "Hi")
+  introduction_with_language("Bleak")
 
 
 
